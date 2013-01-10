1f548741c24306e97a253b3e642f07340737f012
Mark Constable <markc@renta.net>
2013-01-10 15:29:48 +1000

    Added some debugging and one minor security test to deploy.php

--- lib/php/deploy.php
+++ lib/php/deploy.php
@@ -1,6 +1,14 @@
 <?php
 // based on https://gist.github.com/1809044
 
+error_log(var_export($_SERVER, true));
+error_log(var_export($_REQUEST, true));
+
+if ($_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
+  error_log($_SERVER['REMOTE_ADDR']." is not authorized to access deploy.php");
+  exit(1);
+}
+
 // The commands
   $commands = array(
     'echo $PWD',
@@ -19,27 +27,22 @@
 // Run it
     $tmp = shell_exec($command);
 // Output
-    $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-    $output .= htmlentities(trim($tmp)) . "\n";
+    $output .= '
+<span style="color: #6BE234;">$</span> <span style="color: #729FCF;">'.$command.'</span>
+'.htmlentities(trim($tmp));
   }
 
-  // Make it pretty for manual user access (and why not?)
-?>
-<!DOCTYPE HTML>
+echo '<!DOCTYPE HTML>
 <html lang="en-US">
-<head>
-  <meta charset="UTF-8">
-  <title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .  ____________________________
- |/  \|   |        |
-[| <span style="color: #FF0000;">&hearts;  &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /    &copy; oodavid 2012 |
-    |____________________________|
-
-<?php echo $output; ?>
-</pre>
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+  </head>
+  <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+    <h2>Git Deployment Script v0.2</h2>
+    <pre>'.$output.'
+    </pre>
+  </body>
 </html>
+';
+?>
